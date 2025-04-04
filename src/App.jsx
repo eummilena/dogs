c70dcb478d739b801ec811a7ad8cd4b1f6015c53
Milena Mota <80923819+eummilena@users.noreply.github.com>
2025-04-04 15:07:39 -0300

    api

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import Login from './components/login/Login';
+
 import './App.css'
 
 const App = () => {
