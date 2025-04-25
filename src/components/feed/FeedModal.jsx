9cb08ffe71c62f1b3326908e42bd30b8d89daed4
Milena Mota <80923819+eummilena@users.noreply.github.com>
2025-04-25 14:32:48 -0300

    feed modal

--- src/components/feed/FeedModal.jsx
+++ src/components/feed/FeedModal.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import styles from './FeedModal.module.css'
+import useFetch from '../../hooks/useFetch'
+import { PHOTO_GET } from '../../api';
+import Loading from '../Helper/Loading';
+import PhotoContent from '../photo/PhotoContent';
 
-const FeedModal = () => {
+
+const FeedModal = ({ photo, setModalPhoto }) => {
+    const { data, error, loading, request } = useFetch();
+
+    useEffect(() => {
+        const { url, options } = PHOTO_GET(photo.id);
+        request(url, options);
+    }, [photo, request]);
+
+    function handleOutsideClick(event) {
+        console.log('Target:', event.target);
+        console.log('Current:', event.currentTaret);
+
+        if (event.target === event.currentTarget) setModalPhoto(null)
+    }
+
     return (
-        <div>FeedModal</div>
+        <div className={styles.modal} onClick={handleOutsideClick}>
+            {error && <Error error={error} />}
+            {loading && <Loading />}
+            {data && <PhotoContent data={data} />}
+            <img src={photo.scr} />
+        </div>
     )
 }
 
--- src/components/feed/FeedPhotos.jsx
+++ src/components/feed/FeedPhotos.jsx
@@ -5,7 +5,7 @@ import { PHOTOS_GET } from '../../api';
 import Loading from '../Helper/Loading';
 import styles from './FeedPhotos.module.css'
 
-const FeedPhotos = () => {
+const FeedPhotos = ({ setModalPhoto }) => {
 
     const { data, loading, error, request } = useFetch();
 
@@ -26,7 +26,7 @@ const FeedPhotos = () => {
         return (
             <ul className={`${styles.feed} animeLeft`}>
                 {data.map((photo) => (
-                    <FeedPhotosItem key={photo.id} photo={photo} />
+                    <FeedPhotosItem key={photo.id} photo={photo} setModalPhoto={setModalPhoto} />
 
                 ))}
             </ul>
